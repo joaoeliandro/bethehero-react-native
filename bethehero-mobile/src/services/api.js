6651e2cb3e2c2a46e68416f9b2b987f466a82468
João Eliandro Germano <52173525+whistlerip@users.noreply.github.com>
2020-04-03 03:44:34 -0300

    Alteração para acesso ao backend em prod

--- bethehero-mobile/src/services/api.js
+++ bethehero-mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://10.0.1.106:3333'
+    baseURL: 'https://betheheroeliandrobackend.herokuapp.com'
 });
 
 export default api;
